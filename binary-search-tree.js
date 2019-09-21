ccf577552868582992e228478dcdd4840c859a6a
YaseminLi <yaseminli@163.com>
2019-09-21 23:29:27 +0800

    二分搜索树

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,25 +8,20 @@ function BinarySearchTree() {
     };
     //root表示根节点
     let root = null
-    //count表示树中节点的数量
-    let count = 0
-
-    this.isEmpty = function () {
-        return count === 0 ? false : true
+    this.root = function () {
+        return root;
     }
-    this.size = function () {
-        return count;
-    }
-
     //插入节点 递归方法
     this.insert = function (key, value) {
-
-        insert(root, key, value)
+        if (root === null) {
+            root = new Node(key, value)
+            return root
+        }
+        return insert(root, key, value)
         function insert(node, key, value) {
-            if (root === null) {
-                count += 1
-                root = new Node(key, value)
-                return root
+            if (node === null) {
+                node = new Node(key, value)
+                return node
             }
             if (key === node.key) {
                 node.value = value
@@ -39,72 +34,52 @@ function BinarySearchTree() {
         }
     }
 
-    //不用递归实现insert
-    // this.insert = function (key, value) {
-    //     if (root === null) {
-    //    count+=1
-    //         root = new Node(key, value)
-    //     }
-    //     let node = root
-    //     while (node) {
-    //         if(key===node.key){
-    //             node.value=value
-    //         }else if(key>node.key){
-    //             if(!node.right){
-    //                 node.right=new Node(key,value)
-    //                 return
-    //             }
-    //            node=node.right
-    //         }else if(key<node.key){
-    //             if(!node.left){
-    //                 node.left=new Node(key,value)
-    //                 return
-    //             }
-    //             node=node.left
-    //         }
 
-    //     }
-    // }
-
-    //contain方法，是否包含某个key值的节点
+    //contain方法，是否包含key值节点
     this.contain = function (key) {
-        return contain(root, key)
-        function contain(node, key) {
-            if (node === null) {
-                return false
-            }
-            if (node.key === key) {
-                return true
-            } else if (key > node.key) {
-                return contain(node.right, key)
-            } else {
-                return contain(node.left, key)
-            }
-        }
+        console.assert(root, '二叉树为空')
+        return _contain(root, key)
     }
 
+    //contain node为根节点的二叉树中是否包含key值节点
+    function _contain(node, key) {
+        if (node === null) {
+            return false
+        }
+        if (node.key === key) {
+            return true
+        } else if (key > node.key) {
+            return _contain(node.right, key)
+        } else {
+            return _contain(node.left, key)
+        }
+    }
     //查找键值key的value
-    this.serach = function (key) {
-        return search(root, key)
-        function search(node, key) {
-            if (node === null) {
-                return undefined
-            }
-            if (node.key === key) {
-                return node.value
-            } else if (key > node.key) {
-                return search(node.right, key)
-            } else {
-                return search(node.left, key)
-            }
+    this.search = function (key) {
+        console.assert(root, '二叉树为空')
+        let node = _search(root, key)
+        return node ? node.value : `该二叉树中没有key=${key}的节点`
+
+    }
+    //查找node为根节点的二叉树中键值key的节点
+    function _search(node, key) {
+        if (node === null) {
+            return null
+        }
+        if (node.key === key) {
+            return node
+        } else if (key > node.key) {
+            return _search(node.right, key)
+        } else {
+            return _search(node.left, key)
         }
     }
-
     //前序遍历 根左右
-    this.preOrder = function (root) {
+    this.preOrder = function () {
+        console.assert(root, '二叉树为空')
         preOrder(root)
         function preOrder(node) {
-            if (!node) {
+            if (node) {
                 console.log(node.key)
                 preOrder(node.left)
                 preOrder(node.right)
@@ -112,11 +87,12 @@ function BinarySearchTree() {
         }
     }
 
-    //中序遍历 左根右
-    this.inOrder = function (root) {
+    //中序遍历 左根右 从小到大
+    this.inOrder = function () {
+        console.assert(root, '二叉树为空')
         inOrder(root)
         function inOrder(node) {
-            if (!node) {
+            if (node) {
                 inOrder(node.left)
                 console.log(node.key)
                 inOrder(node.right)
@@ -125,10 +101,11 @@ function BinarySearchTree() {
     }
 
     //后序遍历 左右根
-    this.postOrder = function (root) {
+    this.postOrder = function () {
+        console.assert(root, '二叉树为空')
         postOrder(root)
         function postOrder(node) {
-            if (!node) {
+            if (node) {
                 postOrder(node.left)
                 postOrder(node.right)
                 console.log(node.key)
@@ -137,14 +114,15 @@ function BinarySearchTree() {
     }
 
     //层序遍历
-    this.levelOrder = function (root) {
+    this.levelOrder = function () {
+        console.assert(root, '二叉树为空')
         //存储层序遍历的数据
         let arr = []
         //通过一个数组来实现队列，该队列存放需要遍历的节点
         let queue = [root]
         while (queue[0]) {
             let node = queue[0]
-            arr.push(node)
+            arr.push(node.key)
             if (node.left) {
                 queue.push(node.left)
             }
@@ -158,28 +136,98 @@ function BinarySearchTree() {
 
     //查找最小键值
     this.minKey = function () {
-        console.assert(root !== null)
-        let minKey=root.key
-        let node=root
-        while(node){
-            if(node.left){
-                node=node.left
-                minKey=node.key
-            }
+        console.assert(root, '二叉树为空')
+        return _searchMin(root).key
+    }
+    //返回node为根节点的二叉树中最小键值的节点
+    function _searchMin(node) {
+        while (node.left) {
+            node = node.left
         }
-        return minKey
+        return node
     }
     //查找最大键值
     this.maxKey = function () {
-        console.assert(root !== null)
-        let maxKey=root.key
-        let node=root
-        while(node){
-            if(node.right){
-                node=node.right
-                maxKey=node.key
-            }
+        console.assert(root, '二叉树为空')
+        return _searchMax(root).key
+    }
+    //返回node为根节点的二叉树中最大键值的节点
+    function _searchMax(node) {
+        while (node.right) {
+            node = node.right
         }
-        return maxKey
+        return node
+    }
+    //删除最小键值,最小键值没有右节点时直接删除，有右节点时，用右子树代替最小键值节点
+    this.removeMin = function () {
+        console.assert(root, '二叉树为空')
+        _removeMin(root)
+
+    }
+    //删除node为根节点的二叉树中最小键值节点
+    function _removeMin(node) {
+        if (node.left === null) {
+            node = node.right
+            return node
+        }
+        node.left = _removeMin(node.left)
+        return node
+    }
+
+//删除最大键值，最大键值没有左节点时直接删除，有左节点时，用左子树代替最大键值节点
+this.removeMax = function (node) {
+    console.assert(root, '二叉树为空')
+    _removeMax(root)
+}
+//删除node为根节点的二叉树中最大键值节点
+function _removeMax(node) {
+    if (node.right === null) {
+        node = node.left
+        return node
+    }
+    node.right = _removeMax(node.right)
+    return node
+}
+//删除键值为key的节点
+this.remove = function (key) {
+    console.assert(root, '二叉树为空')
+    _remove(root, key)
+}
+//删除node为根节点的二叉树中键值为key的节点
+function _remove(node, key) {
+    if (key < node.key) {
+        node.left = _remove(node.left, key)
+        return node
+    }
+    if (key > node.key) {
+        node.right = _remove(node.right, key)
+        return node
+    }
+    if (key === node.key) {
+        //没有子节点时，直接删除该节点
+        if (!node.left && !node.right) {
+            node = null
+        }
+        //只有一个子节点时
+        if (node.left === null || node.right === null) {
+            node = node.left || node.right
+        }
+        //有两个子节点时
+        if (node.left && node.right) {
+            //找出右子树中的最小键值节点
+            let min = _searchMin(node.right)
+            node.key = min.key
+            node.value = min.value
+            //删除右子树中的最小键值节点
+            node.right=_removeMin(node.right)
+        }
+        return node
+
+
     }
 }
+
+
+}
+
+module.exports = { BinarySearchTree }
